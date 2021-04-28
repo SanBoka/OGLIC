f02e5d2dfb150bbdf1d0182023b00c54d49de1ec
SanBoka <66211113+SanBoka@users.noreply.github.com>
2021-04-28 08:10:38 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,543 @@
+﻿//------------------------------------------------------------------->>by : MeTuBe Plus
+
+const Discord = require("discord.js");
+const myid = ['728045009483464766'];
+const developers = ['728045009483464766'];
+const prefix = ['Zabikbiiirhhh'];
+const cmd = require("node-cmd")
+const client = new Discord.Client();//by : MeTuBe
+const client2 = new Discord.Client();//by : MeTuBe
+const client3 = new Discord.Client();//by : MeTuBe
+const client4 = new Discord.Client();//by : MeTuBe
+const client5 = new Discord.Client();//by : MeTuBe
+const client6 = new Discord.Client();//by : MeTuBe
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client.login("ODM2MzI0NDg1NjM2MTYxNTQ2.YIc5pA.CGbf_p6WfmiQPg_lXxgcx3Ng2-E");
+//OG Loc
+client2.login("ODM2MzE3ODczMzUzNTIzMjMx.YIcRVw.zlLwaZkjASMOWLEP3bEhMUvbhD8");
+// Tuffy
+client3.login("ODM2MDc3ODEyNzExNTU1MTI0.YIYwww.j7uWXH1mA3FIhVLiW4Ssl_gi9Ks"); 
+// Florian
+client4.login("ODM2MDc1NzEzNjY4NTc5MzM5.YIYvNg.S1ChSTLEfpRn8Ma5zrpCbOPO-HA"); 
+// Farah
+client5.login("ODM2MDc0Mjk2OTc5MjI2Njg1.YIYtgA.OOBO_Y-GVsb_LUGTUnMx-z2Zi00"); 
+// Kenza 
+client6.login("ODM2MDcxNTY0NzUxMDExODYy.YIYroA.l9oXEj3JzrzumJ-XCH19PxYALnw"); 
+// Diana
+client7.login("ODM1OTIzMDIwNDIxMjY3NDgy.YIYkpg.yBNQ0hEJJ_1DRGnYOWADbt3smwg"); 
+// Luna
+client8.login("ODM2MzY0MTQyNjcwNzc0Mzcy.YIdCzg.PpZq_5Vk3aTzq7K1foxmXQR0G2U");
+//3bad l7rira
+client9.login("ODM2MzczNTI1Mjk1MjY3ODcw.YIdD7Q.d8Q8ALhwsqX_C-LiAyJaLuL2kgM");
+//GS4Life
+client10.login("ODM2Mzc0ODY0MzgyMDAxMTgz.YIdFkg.9N_7qmSrTZcz0ZVgx3vsh_Ewqaw");
+//Rijal 
+client11.login("ODM2Mzc2ODI1MjM1MjQzMDA4.YIdHEQ.O9360zQvX0qq1djE8JnnZkza1X8");
+//Virus
+client12.login("ODM2Mzc4MzYwMzcyMjY0OTkw.YIdIbA.ZmevLwkLlci8GPP3nqS9pACHubs");
+//Big Bear
+
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+  if (message.content === 'd1') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c1'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r1'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if(message.content ===  prefix+'u1'){
+message.channel.send("metube")
+  }  
+  if (message.content === 'p1') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s1")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+ //------------------------------------------------------------------->>by : MeTuBe Plus
+ 
+client2.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+  if (message.content === 'd2') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c2'){
+message.channel.send('#credits')
+  }
+   if(message.content === 'r2'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p2') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s2")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+ 
+ //------------------------------------------------------------------->>by : MeTuBe Plus
+ 
+client3.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+  if (message.content === 'd3') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c3'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r3'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p3') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s3")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+ 
+ //------------------------------------------------------------------->>by : MeTuBe Plus
+ 
+client4.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+  if (message.content === 'd4') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c4'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r4'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p4') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s4")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+ 
+ //------------------------------------------------------------------->>by : MeTuBe Plus
+ 
+client5.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+  if (message.content === 'd5') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c5'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r5'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p5') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s5")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client6.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+  if (message.content === 'd6') {
+message.channel.send('#daily')
+  }
+  if(message.content === 'c6'){
+message.channel.send('#credits')
+  }
+  if(message.content === 'r6'){
+message.channel.send("#rep "+"<@" + myid + ">")
+  }
+  if (message.content === 'p6') {
+message.channel.send('#profile')
+  }
+  if(message.content.startsWith("s6")) {
+    var text = message.content.split(' ').slice(1).join(' ');
+     message.channel.send(text);
+  }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client.on("ready", () => {
+  console.log('Bot created by : metube plus')
+})
+
+client.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``1`` ")
+    }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client2.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``2`` ")
+    }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client3.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``3`` ")
+    }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client4.on('message', message => {
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``4`` ")
+    }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client5.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``5`` ")
+    }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client6.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+    if(message.content === prefix+'number'){
+        message.channel.send(" My Token Is ``6`` ")
+    }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client3.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client4.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+//by : MeTuBe
+client5.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');//by : MeTuBe
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {//by : MeTuBe
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)//by : MeTuBe
+          .then(m => {
+            count++;
+          })//by : MeTuBe
+          
+        }
+      }//by : MeTuBe
+});
+
+client6.on('message', message => {
+var argresult = message.content.split(` `).slice(1).join(' ');
+if (!developers.includes(message.author.id)) return;
+if (message.content === prefix+'fast') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 30000; x++) {
+        message.channel.send(`**??? ???? ??? ?? ??? **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+client.on('message', message => {
+if (message.content === prefix+'slow') {
+        let letters = "AaBbCcDFfGgHh-MmNnOoP-pQqRrSsT-metube-YT-tUuVvWw-XxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);//by : MeTuBe
+        let last = "";
+        let messages = 0;
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);//by : MeTuBe
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)]; 
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;//by : MeTuBe
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;//by : MeTuBe
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }
+                }
+            }
+        }, 6000);
+}
+}); 
+
+client2.on('message', message => {
+if (message.content === prefix+'slow') {
+        let letters = "AaBbCcDFfGgHh-MmNnOoP-pQqRrSsT-metube-YT-tUuVvWw-XxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);
+        let last = "";//by : MeTuBe
+        let messages = 0;
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)]; 
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;
+                        last = `${str} ${spaces.slice(str.length + 1)}`;//by : MeTuBe
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }//by : MeTuBe
+                }
+            }
+        }, 6000);
+}
+}); 
+
+client3.on('message', message => {
+if (message.content === prefix+'slow') {
+        let letters = "AaBbCcDFfGgHh-MmNnOoP-pQqRrSsT-metube-YT-tUuVvWw-XxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);
+        let last = "";
+        let messages = 0;
+ 
+        setInterval(() => {//by : MeTuBe
+            let str = "";
+            let num = Math.floor(Math.random() * 10);
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)]; 
+ //by : MeTuBe
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;//by : MeTuBe
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }
+                }
+            }
+        }, 6000);
+}
+}); 
+
+client4.on('message', message => {
+if (message.content === prefix+'slow') {
+        let letters = "AaBbCcDFfGgHh-MmNnOoP-pQqRrSsT-metube-YT-tUuVvWw-XxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);//by : MeTuBe
+        let last = "";
+        let messages = 0;
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);
+            for(let i = 0; i < num; i++) {//by : MeTuBe
+                str += letters[Math.floor(Math.random() * letters.length)]; 
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
+                        last = spaced;
+                        return message.channel.send(spaced);//by : MeTuBe
+                    }
+                }
+            }
+        }, 6000);
+}
+}); 
+
+client5.on('message', message => {
+if (message.content === prefix+'slow') {
+        let letters = "AaBbCcDFfGgHh-MmNnOoP-pQqRrSsT-metube-YT-tUuVvWw-XxYyZz0123456789".split("");
+        let spaces = " ".repeat(10);
+        let last = "";
+        let messages = 0;//by : MeTuBe
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)]; //by : MeTuBe
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }
+                }
+            }
+        }, 6000);
+}
+}); 
+
+client6.on('message', message => {
+if (message.content === prefix+'slow') {
+        let letters = "AaBbCcDFfGgHh-MmNnOoP-pQqRrSsT-metube-YT-tUuVvWw-XxYyZz0123456789".split("");//by : MeTuBe
+        let spaces = " ".repeat(10);
+        let last = "";
+        let messages = 0;
+ 
+        setInterval(() => {
+            let str = "";
+            let num = Math.floor(Math.random() * 10);//by : MeTuBe
+            for(let i = 0; i < num; i++) {
+                str += letters[Math.floor(Math.random() * letters.length)]; 
+ 
+                if(i === num - 1) {
+                    if(last === "") {
+                        messages += 1;//by : MeTuBe
+                        last = `${str} ${spaces.slice(str.length + 1)}`;
+                        return message.channel.send(`${str} ${spaces.slice(str.length + 1)}${messages}`);
+                    } else {
+                        messages += 1;
+                        let spaced = `${str} ${spaces.slice(str.length + 1)}${messages}`;//by : MeTuBe
+                        last = spaced;
+                        return message.channel.send(spaced);
+                    }
+                }
+            }
+        }, 6000);
+}//by : MeTuBe
+}); 
+
+//------------------------------------------------------------------->>by : MeTuBe Plus
+
+const express = require("express");
+const app = express();//by : MeTuBe
+
+app.listen(() => console.log("Bot created by metube"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());//by : MeTuBe
+});
+
+
+
